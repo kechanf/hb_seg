8c5e13f844aa950a630c0ebb0737f91db5424904
kfchen <kechanf@gmail.com>
2024-05-22 13:29:51 +0800

    调试

--- nnUNet/nnunetv2/training/dataloading/data_loader_3d.py
+++ nnUNet/nnunetv2/training/dataloading/data_loader_3d.py
@@ -1,26 +1,12 @@
 import numpy as np
 from nnunetv2.training.dataloading.base_data_loader import nnUNetDataLoaderBase
 from nnunetv2.training.dataloading.nnunet_dataset import nnUNetDataset
-import cc3d
-import os
-import matplotlib.pyplot as plt
 
-def save_mip_image(image, filename='', temp_dir="/data/kfchen/nnUNet/temp_mip"):
-    if(filename == ''):
-        filename = str(np.random.randint(0, 100000)) + '.png'
-    mip = np.max(image, axis=0)
-    plt.imshow(mip, cmap='gray')
-    plt.axis('off')
-    plt.title('Maximum Intensity Projection')
-    plt.savefig(os.path.join(temp_dir, filename))
-    plt.close()
-    print(f"MIP图已保存为 {filename}")
 
 class nnUNetDataLoader3D(nnUNetDataLoaderBase):
     def generate_train_batch(self):
         selected_keys = self.get_indices()
         # preallocate memory for data and seg
-        # print(f"self.data_shape, self.seg_shape {self.data_shape, self.seg_shape} in nnUNetDataLoader3D generate_train_batch")
         data_all = np.zeros(self.data_shape, dtype=np.float32)
         seg_all = np.zeros(self.seg_shape, dtype=np.int16)
         case_properties = []
@@ -31,15 +17,7 @@ class nnUNetDataLoader3D(nnUNetDataLoaderBase):
             force_fg = self.get_do_oversample(j)
 
             data, seg, properties = self._data.load_case(i)
-            _, cc_num = cc3d.connected_components(seg[0].copy(), connectivity=26, return_N=True)
-            if (cc_num > 1):
-                print(f"cc_num: {cc_num} in generate_train_batch, shape:{seg[0].shape}")
-            # print(f"data.shape, seg.shape {data.shape, seg.shape} in nnUNetDataLoader3D load_case")
             case_properties.append(properties)
-
-            # print(f"seg.shape: {seg.shape} in generate_train_batch")
-            # _, cc_num = cc3d.connected_components(seg[0].copy(), connectivity=26, return_N=True)
-            # print(f"cc_num: {cc_num} in generate_train_batch")
 
             # If we are doing the cascade then the segmentation from the previous stage will already have been loaded by
             # self._data.load_case(i) (see nnUNetDataset.load_case)
@@ -59,48 +37,15 @@ class nnUNetDataLoader3D(nnUNetDataLoaderBase):
             # be padded with -1 constant whereas seg_from_previous_stage needs to be padded with 0s (we could also
             # remove label -1 in the data augmentation but this way it is less error prone)
             this_slice = tuple([slice(0, data.shape[0])] + [slice(i, j) for i, j in zip(valid_bbox_lbs, valid_bbox_ubs)])
-            # data = data[this_slice]
+            data = data[this_slice]
 
-            _, cc_num = cc3d.connected_components(seg[0].copy(), connectivity=26, return_N=True)
-            if (cc_num > 1):
-                print(f"cc_num: {cc_num} in generate_train_batch step 1, shape:{seg[0].shape}")
-
-            # print(seg.shape)
             this_slice = tuple([slice(0, seg.shape[0])] + [slice(i, j) for i, j in zip(valid_bbox_lbs, valid_bbox_ubs)])
-            # seg = seg[this_slice]
+            seg = seg[this_slice]
 
-
-            _, cc_num = cc3d.connected_components(seg[0].copy(), connectivity=26, return_N=True)
-            if (cc_num > 1):
-                print(this_slice)
-                print(f"cc_num: {cc_num} in generate_train_batch step 2, shape:{seg[0].shape}")
-
-            # padding = [(-min(0, bbox_lbs[i]), max(bbox_ubs[i] - shape[i], 0)) for i in range(dim)]
-            # padding = ((0, 0), *padding)
-            # print(padding)
-            patch_size = self.patch_size
-            padding = [(max(0, (p - s) // 2), max(0, (p - s + 1) // 2)) for s, p in zip(data[0].shape, patch_size)]
+            padding = [(-min(0, bbox_lbs[i]), max(bbox_ubs[i] - shape[i], 0)) for i in range(dim)]
             padding = ((0, 0), *padding)
-            # print(data.shape, patch_size, padding)
-            # print(padding)
             data_all[j] = np.pad(data, padding, 'constant', constant_values=0)
-            seg_all[j] = np.pad(seg, padding, 'constant', constant_values=0)
-            seg_all[j] = np.where(seg_all[j] > 0, 1, 0)
-            # print(f"data_all[j].shape, seg_all[j].shape {data_all[j].shape, seg_all[j].shape} in nnUNetDataLoader3D")
-
-            # # print(f"seg.shape: {seg.shape} in generate_train_batch")
-            # _, cc_num = cc3d.connected_components(seg_all[j][0].copy(), connectivity=26, return_N=True)
-            # if(cc_num>1):
-            #     print(f"cc_num: {cc_num} in generate_train_batch")
-            #     save_mip_image(seg_all[j][0])
-            #     data_all[j] = np.zeros_like(data_all[j])
-            #     seg_all[j] = np.zeros_like(seg_all[j])
-
-            _, cc_num = cc3d.connected_components(seg_all[j][0].copy(), connectivity=26, return_N=True)
-            if(cc_num>1):
-                print(f"cc_num: {cc_num} in generate_train_batch after padding")
-
-
+            seg_all[j] = np.pad(seg, padding, 'constant', constant_values=-1)
 
         return {'data': data_all, 'seg': seg_all, 'properties': case_properties, 'keys': selected_keys}
 
